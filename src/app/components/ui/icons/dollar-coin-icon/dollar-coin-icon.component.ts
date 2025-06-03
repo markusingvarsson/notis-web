0626560de6f267e6c8c02e73b88032a280a1da52
markusingvarsson <marckhuz@msn.com>
2025-06-03 20:06:00 +0200

    refactor svgs into icons

---
+++ src/app/components/ui/icons/dollar-coin-icon/dollar-coin-icon.component.ts
@@ -0,0 +1,57 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dollar-coin-icon',
+  standalone: true,
+  imports: [],
+  template: `
+    <svg
+      [style.width.px]="size()"
+      [style.height.px]="size()"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="dolar_coin" data-name="dolar coin">
+        <path
+          d="M22.5,12A10.5,10.5,0,1,1,12,1.5,10.5,10.5,0,0,1,22.5,12"
+          [style.stroke]="color()"
+          stroke-linecap="square"
+          stroke-miterlimit="10"
+          stroke-width="1.91px"
+        ></path>
+        <path
+          d="M9.14,15.82H13a1.91,1.91,0,0,0,1.91-1.91h0A1.9,1.9,0,0,0,13,12h-1.9a1.9,1.9,0,0,1-1.91-1.91h0a1.91,1.91,0,0,1,1.91-1.91h3.81"
+          [style.stroke]="color()"
+          stroke-linecap="square"
+          stroke-miterlimit="10"
+          stroke-width="1.91px"
+        ></path>
+        <line
+          x1="12"
+          y1="6.27"
+          x2="12"
+          y2="8.18"
+          [style.stroke]="color()"
+          stroke-linecap="square"
+          stroke-miterlimit="10"
+          stroke-width="1.91px"
+        ></line>
+        <line
+          x1="12"
+          y1="15.82"
+          x2="12"
+          y2="17.73"
+          [style.stroke]="color()"
+          stroke-linecap="square"
+          stroke-miterlimit="10"
+          stroke-width="1.91px"
+        ></line>
+      </g>
+    </svg>
+  `,
+})
+export class DollarCoinIconComponent {
+  readonly size = input<number>(48);
+  readonly color = input<string>('var(--tw-primary)');
+}
--- src/app/domain/landing/components/usp/usp.component.ts
+++ src/app/domain/landing/components/usp/usp.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { DollarCoinIconComponent } from '../../../../components/ui/icons/dollar-coin-icon/dollar-coin-icon.component';
+import { ShieldHeartIconComponent } from '../../../../components/ui/icons/shield-heart-icon/shield-heart-icon.component';
+import { GithubLogoIconComponent } from '../../../../components/ui/icons/github-logo-icon/github-logo-icon.component';
+import { GithubLinkIconComponent } from '../../../../components/ui/icons/github-link-icon/github-link-icon.component';
 
 @Component({
   selector: 'app-usp',
-  imports: [],
+  standalone: true,
+  imports: [
+    DollarCoinIconComponent,
+    ShieldHeartIconComponent,
+    GithubLogoIconComponent,
+    GithubLinkIconComponent,
+  ],
   templateUrl: './usp.component.html',
   styleUrl: './usp.component.scss',
 })
