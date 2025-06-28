18ee208b6020ec12a8ad667fae9f8402a3e95c08
markusingvarsson <marckhuz@msn.com>
2025-06-28 22:58:13 +0200

    improve save icon

--- src/app/components/ui/icons/save-icon/save-icon.component.ts
+++ src/app/components/ui/icons/save-icon/save-icon.component.ts
@@ -13,23 +13,23 @@ import { Component, input, ChangeDetectionStrategy } from '@angular/core';
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M17 21H7C5.89543 21 5 20.1046 5 19V5C5 3.89543 5.89543 3 7 3H14L19 8V19C19 20.1046 18.1046 21 17 21Z"
+        d="M19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H16L21 8V19C21 20.1046 20.1046 21 19 21Z"
         [style.stroke]="color()"
-        stroke-width="2"
+        stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
-        d="M15 3V8H19"
+        d="M17 21V13H7V21"
         [style.stroke]="color()"
-        stroke-width="2"
+        stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
-        d="M9 9H15"
+        d="M7 3V8H15"
         [style.stroke]="color()"
-        stroke-width="2"
+        stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
